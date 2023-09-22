d10df23d0fb1d6d9fefe91df235f214ca3adbe86
Smyslov.Aleksey <a.smisloff@gmail.com>
2023-09-22 08:57:22 +0300

    Alert

---
+++ src/test/java/ru/vniizht/asuter/autotest/Alert.java
@@ -0,0 +1,30 @@
+package ru.vniizht.asuter.autotest;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import java.util.Objects;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public record Alert(SelenideElement modal, SelenideElement h3, SelenideElement closeButton) {
+
+    public static Alert find() {
+        SelenideElement modal = $("#openModal");
+        SelenideElement h3 = modal.$(By.tagName("h3"));
+        SelenideElement closeButton = modal.$(By.tagName("button"));
+        return new Alert(modal, h3, closeButton);
+    }
+
+    public boolean isVisible() {
+        return !Objects.requireNonNull(modal.getAttribute("class")).contains("modalClosed");
+    }
+
+    public String getText() {
+        return h3.text();
+    }
+
+    public void close() {
+        closeButton.click();
+    }
+}
